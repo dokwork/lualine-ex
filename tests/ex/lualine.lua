23e676f07dd5de7c2a92197044345264f903c1e0
Vladimir Popov <vladimir@dokwork.ru>
2023-11-05 15:56:34 +0400

    chore: improve tool for tests

--- tests/ex/lualine.lua
+++ tests/ex/lualine.lua
@@ -39,12 +39,28 @@ function M.get_gui_color(group, attr)
     return vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID(group)), attr, 'gui')
 end
 
---- Compares two colors. The first color can be presented as a name or #rrggbb. The second
---- color must be in #rrggbb format.
-function M.eq_colors(expected_color, actual_rgb, msg)
-    local expected_rgb = expected_color:find('#') and expected_color
-        or uc.color_name2rgb(expected_color)
-    assert.are.equal(expected_rgb, actual_rgb, msg)
+--- Compares two colors. The first color can be presented as a name or #rrggbb,
+--- or a table with `fg` and `bg` properties with same format.
+--- The second color must be in #rrggbb format, or a table with `fg` and `bg` properties
+--- strictly in #rrggbb format.
+function M.eq_colors(expected, actual, msg)
+    if type(expected) ~= type(actual) then
+        error(
+            string.format(
+                'Expected and actual colors have different types: %s, %s',
+                vim.inspect(expected),
+                vim.inspect(actual)
+            )
+        )
+    end
+    if type(expected) == 'string' then
+        local expected_rgb = expected:find('#') and expected or uc.color_name2rgb(expected)
+        assert.are.equal(expected_rgb, actual, msg)
+    end
+    if type(expected) == 'table' then
+        M.eq_colors(expected.fg, actual.fg)
+        M.eq_colors(expected.bg, actual.bg)
+    end
 end
 
 ---@class ComponentTable
@@ -55,10 +71,14 @@ end
 ---@field color? Color
 ---@field value string
 
----@return ComponentTable
+---Gets a string with rendered component and match its properties.
+---@param rendered_component string
+---@param opts table inside information about component's options.
+---@return ComponentTable # matched component.
 function M.match_rendered_component(rendered_component, opts)
     local is_right_icon = opts and opts.icon and opts.icon.align == 'right'
     local p_hl = '%%#([%w_]+)#'
+    local p_icon = '(%S+)'
     local p_value = '(.*)'
     local p_padding = '%s*'
     local t = {}
@@ -94,9 +114,9 @@ function M.match_rendered_component(rendered_component, opts)
     if not t.icon then
         local value, icon
         if is_right_icon then
-            value, icon = string.match(t.value, '(.*) (.+)')
+            value, icon = string.match(t.value, p_value .. p_padding .. p_icon)
         else
-            icon, value = string.match(t.value, '(.+) (.*)')
+            icon, value = string.match(t.value, p_icon .. p_padding .. p_value)
         end
         t.icon = icon
         t.value = value or t.value
@@ -104,6 +124,10 @@ function M.match_rendered_component(rendered_component, opts)
     return t
 end
 
+--- Builds and draws the component.
+---@param component string|table string should contain the name of the component;
+---         the table should be result of the `init_component` method.
+---@param opts table custom options for the drawn component.
 ---@return string
 function M.render_component(component, opts)
     if type(component) == 'string' then
@@ -113,6 +137,8 @@ function M.render_component(component, opts)
     return component:draw(hl, true)
 end
 
+---Draws and matches the component. See {render_component} and {match_rendered_component}.
+---@param component string | table
 ---@return ComponentTable
 function M.match_component(component, opts)
     local rendered_component = M.render_component(component, opts)
